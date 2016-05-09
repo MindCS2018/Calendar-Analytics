cd1bead0cd60c6386b9ea930de0a062d8bc13c93
Megan <meg.heskett@gmail.com>
2016-05-09 15:08:18 -0700

    added event variables

--- quickstart.py
+++ quickstart.py
@@ -22,6 +22,13 @@ CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Google Calendar API Python Quickstart'
 
 
+# def oauth():
+#        """Your application must be authenticated, the user must
+#     grant access for your application, and the user must be authenticated
+#     in order to grant that access.
+#     """
+
+
 def get_credentials():
     """Gets valid user credentials from storage.
 
@@ -31,6 +38,7 @@ def get_credentials():
     Returns:
         Credentials, the obtained credential.
     """
+
     home_dir = os.path.expanduser('~/Desktop/Hackbright/proj')
     credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
@@ -45,10 +53,13 @@ def get_credentials():
         flow.user_agent = APPLICATION_NAME
         if flags:
             credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
+
+        # Needed only for compatibility with Python 2.6
+        else:
             credentials = tools.run(flow, store)
         print('Storing credentials to ' + credential_path)
     return credentials
+
 
 def main():
     """Shows basic usage of the Google Calendar API.
@@ -57,10 +68,18 @@ def main():
     10 events on the user's calendar.
     """
 
-    #calls get credentials function
+    # calls get_credentials function
     credentials = get_credentials()
 
+    # Creates an httplib2.Http object to handle HTTP requests and authorizes it
     http = credentials.authorize(httplib2.Http())
+
+    # The apiclient.discovery.build() function returns an instance of an API service
+    # object can be used to make API calls. The object is constructed with
+    # methods specific to the calendar API. The arguments provided are:
+    #   name of the API ('calendar')
+    #   version of the API you are using ('v3')
+    #   authorized httplib2.Http() object that can be used for API calls
 
     service = discovery.build('calendar', 'v3', http=http)
 
@@ -68,9 +87,16 @@ def main():
     now = datetime.datetime.utcnow().isoformat() + 'Z'
     print('Getting the upcoming 10 events')
 
-    # calendarResult = service.calendarList() #<googleapiclient.discovery.Resource object at 0x1032201d0>
-    # calendarResult = service.calendarList().list() #<googleapiclient.http.HttpRequest object at 0x103120390>
-    # calendarResult = service.calendarList().list().execute() # dictionary
+    # calendarResult = service.calendarList()
+    """Collection object: <googleapiclient.discovery.Resource object at 0x1032201d0>"""
+
+    # calendarResult = service.calendarList().list()
+    """Http request object: <googleapiclient.http.HttpRequest object at 0x103120390>"""
+
+    # calendarResult = service.calendarList().list().execute()
+    """Creating a request does not actually call the API. To execute the request
+    and get a response, call the execute() function. The response is a Python
+    object built from the JSON response sent by the API server."""
 
     # eventsResult is a dictionary
     eventsResult = service.events().list(
@@ -94,9 +120,38 @@ def main():
         # if dateTime does not exist, bind the date
         start = event['start'].get('dateTime', event['start'].get('date'))
         end = event['end'].get('dateTime', event['start'].get('date'))
+        creator = event['creator'].get('email', [])
+        kind = event['kind']
+        status = event['status']
+        summary = event['summary']
+        event_id = event['id']
+        created_dateTime = event['created']
+        updated_dateTime = event['updated']
 
         # print variables and the value to the summary key
-        print(start, end, event['summary'])
+        print ("******")
+        print ("Event Title: ", summary)
+        print ("Event id:", event_id)
+        print ("Created: ", created_dateTime)
+        print ("Updated ", updated_dateTime)
+        print ("Start DateTime: ", start)
+        print ("End DateTime: ", end)
+        print ("Event Creator: ", creator)
+        print ("Kind: ", kind)
+        print ("Status: ", status)
+
+        if 'attendees' in event:
+            attendees = event['attendees']
+
+            for attendee in attendees:
+                if 'resource' in attendee:
+                    display_name = attendee['displayName']
+                    print ("Where: ", display_name)
+
+                #TODO: remove primary email from guest
+                else:
+                    guest = attendee['email']
+                    print ("Who: ", guest)
 
 
 if __name__ == '__main__':
