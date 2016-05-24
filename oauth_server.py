7dc4de7c79e24e42bc56b46d92f807d2cf5983aa
Megan <meg.heskett@gmail.com>
2016-05-24 15:33:41 -0700

    added error handling for calendars with no meetings

--- oauth_server.py
+++ oauth_server.py
@@ -11,7 +11,6 @@ from datetime import datetime, timedelta
 import logging
 from seed import seed_user, seed_calendars, seed_events
 import json
-import numpy
 
 
 app = Flask(__name__)
@@ -19,41 +18,6 @@ app.secret_key = os.environ["FLASK_APP_KEY"]
 app.jinja_env.undefined = StrictUndefined
 
 # import pdb; pdb.set_trace()
-
-
-def events_query():
-
-    return Event.query.all()
-
-
-def next_week():
-    """Pulls events for next week from db"""
-
-    # datetime with timezone
-    now = datetime.now()
-
-    # need to only pull events from calendars associated with user_id
-    next_week = now + timedelta(weeks=1)
-
-    return Event.query.filter(Event.start < next_week,
-                              Event.summary.like('%WFH%')).all()
-
-
-# def out_for_lunch():
-#     """Out for lunch next week"""
-
-#     now = datetime.now()
-#     lunch_start = datetime.time(12)
-#     lunch_end = datetime.time(13)
-
-#     next_week = now + timedelta(weeks=1)
-
-#     ofl_next_week = Event.query.filter(Event.start < next_week,
-#                                        Event.start_time < lunch_start,
-#                                        Event.end_time > lunch_end,
-#                                        (Event.summary.like('%site%') | Event.summary.like('%ooo%'))).all()
-
-#     return ofl_next_week
 
 
 @app.route('/')
@@ -83,11 +47,11 @@ def oauth2callback():
         auth_code = request.args.get('code')
         credentials = flow.step2_exchange(auth_code)
         session['credentials'] = credentials.to_json()
-        return redirect(url_for('calendar'))
+        return redirect(url_for('oauth'))
 
 
-@app.route('/calendar')
-def calendar():
+@app.route('/oauth')
+def oauth():
 
     if 'credentials' not in session:
         return redirect(url_for('oauth2callback'))
@@ -97,7 +61,6 @@ def calendar():
         return redirect(url_for('oauth2callback'))
     else:
         # creates api service objects
-        # print session['credentials']
         http_auth = credentials.authorize(httplib2.Http())
         people_service = build('people', 'v1', http_auth)
         calendar_service = build('calendar', 'v3', http_auth)
@@ -105,9 +68,10 @@ def calendar():
 
     # profile api call
     profile = people_service.people().get(resourceName='people/me').execute()
-    # user_id = profile['names'][0]['metadata']['source']['id']
+
     cred_dict = json.loads(session['credentials'])
     user_id = cred_dict['id_token']['sub']
+
     session["user_id"] = user_id
 
     # calendars api call
@@ -160,13 +124,12 @@ def calendar():
 
     credentials.revoke(httplib2.Http())  # for demo purposes
 
-    return redirect("/next")
+    return redirect("/calendars")
 
 
-@app.route("/next")
-def next():
+@app.route("/calendars")
+def calendars():
 
-    # get list of calendar ids associated with a user from database
     user_id = session['user_id']
 
     all_calendars = UserCal.query.filter_by(user_id=user_id).all()
@@ -175,88 +138,15 @@ def next():
     for calendar in all_calendars:
         calendar_options.append(calendar.calendar.summary)
 
-    # calendar_options = []
-    # for calendar_obj in calendarsResult['items']:
-    #     calendar_options.append(calendar_obj['summary'])
-
     return render_template('calendars.html',
                            calendar_options=calendar_options)
 
 
-# @app.route('/mapper.json')
-# def build_mapper():
-
-#     # selected = request.form.getlist('calendar')
-#     print("**selected**")
-#     # print selected
-#     print("****")
-
-#     # list of calendar objects
-#     # TODO: pull from choose calendars page
-#     calendar_objs = Calendar.query.all()
-
-#     # creates mapper object
-#     mpr = {}
-#     x = 0
-#     for calendar in calendar_objs:
-#         calendar_summary = (calendar.calendar_id.split("@")[0]).title()
-#         mpr[calendar_summary] = {"id": x,
-#                                  "name": calendar_summary}
-#         x += 1
-#     print("**mpr**")
-#     print mpr
-#     print("****")
-
-#     print mpr
-
-#     ids = set()
-
-#     for event in events:
-#         for calevent in event.calevents:
-#             ids.add(calevent.calendar_id)
-
-#     n = str(len(ids))
-
-#     empty_matrix = numpy.zeros(shape=(n, n))
-
-    # return jsonify(mpr)
-
-
-# @app.route('/matrix.json')
-# def build_matrix():
-
-#     n = 8
-
-#     empty_matrix = numpy.zeros(shape=(n, n))
-
-#     matrix_data = {"data": {"date": empty_matrix}}
-
-#     return jsonify(matrix_data)
-
-
-# @app.route('/events.json')
-# def build_events():
-
-#     events = events_query()
-
-#     chord_data = {"data": [event.serialize() for event in events]}
-
-#     return jsonify(chord_data)
-
-
 @app.route('/dashboard', methods=['POST'])
-def d3():
+def dashboard():
 
-    # creating json here
-
+    # choice of calendars
     selected = request.form.getlist('calendar')
-    print("**selected**")
-    print selected
-    print("****")
-
-    # list of calendar objects
-    # TODO: pull from choose calendars page
-    # calendar_objs = Calendar.query.all()
 
     # creates mapper object
     mpr = {}
@@ -267,97 +157,22 @@ def d3():
                                  "name": calendar_summary}
         x += 1
 
-    # TODO: change query to reflect only events that are
-    # associated with chosen calendar_ids
-
-    mpr = json.dumps(mpr)
-
-    # TODO: pull list of events associated with calendar_ids
-
-    # for calendar in selected:
-
+    # creates event object
     event_objs = set()
 
     for cal in selected:
         for calevent in CalEvent.query.filter_by(calendar_id=cal).all():
             event_objs.add(calevent.event)
 
-    # event_objs = events_query()
     event_objs = list(event_objs)
-
     chord_data = {"data": [event_obj.serialize() for event_obj in event_objs]}
+
+    mpr = json.dumps(mpr)
     chord_data = json.dumps(chord_data)
 
-    # return jsonify(chord_data)
-
-    return render_template('meetings.html',
+    return render_template('dashboard.html',
                            mpr=mpr,
                            chord_data=chord_data)
-
-
-# @app.route('/weekly.json')
-# def weekly_data():
-#     """Creates data for weekly chart"""
-
-#     wfh_next_week = next_week()
-
-#     week = {}
-
-#     for event in wfh_next_week:
-#         day = event.start.weekday()
-#         for calevent in event.calevents:
-#             week.setdefault(day, set()).add(calevent.calendar_id)
-
-#     data = [0, 0, 0, 0, 0, 0, 0]
-
-#     for key, value in week.iteritems():
-#         data[key] = len(value)
-
-#     data_dict = {
-#         "labels": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-#         "datasets": [
-#             {
-#                 "label": "Out of Office",
-#                 "fillColor": "rgba(151,187,205,0.2)",
-#                 "strokeColor": "rgba(151,187,205,1)",
-#                 "pointColor": "rgba(151,187,205,1)",
-#                 "pointStrokeColor": "#fff",
-#                 "pointHighlightFill": "#fff",
-#                 "pointHighlightStroke": "rgba(151,187,205,1)",
-#                 "data": data
-#             },
-#             {
-#                 "label": "Out for Lunch",
-#                 "fillColor": "rgba(219,186,52,0.4)",
-#                 "strokeColor": "rgba(220,220,220,1)",
-#                 "pointColor": "rgba(220,220,220,1)",
-#                 "pointStrokeColor": "#fff",
-#                 "pointHighlightFill": "#fff",
-#                 "pointHighlightStroke": "rgba(151,187,205,1)",
-#                 "data": [0, 1, 1, 1, 3, 0, 0]
-#             },
-#         ]
-#     }
-#     return jsonify(data_dict)
-
-
-@app.route('/chart')
-def chart():
-
-    # db query
-    wfh_next_week = next_week()
-
-    return render_template('chart.html',
-                           wfh_next_week=wfh_next_week)
-
-
-@app.route('/breakdown')
-def breakdown():
-
-    wfh_next_week = next_week()
-
-    return render_template('breakdown.html',
-                           wfh_next_week=wfh_next_week)
 
 
 @app.route('/logout')
