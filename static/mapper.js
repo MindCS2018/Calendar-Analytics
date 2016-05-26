0dd613189a889b725278c8db5077da3912e39329
Megan <meg.heskett@gmail.com>
2016-05-26 14:15:24 -0700

    improved variable names

--- static/mapper.js
+++ static/mapper.js
@@ -3,33 +3,33 @@ function chordRdr (matrix, mpr) {
   return function (d) {
   // console.log(d);
 
-    // empty objects
-    var i,j,s,t,g,m = {};
+    var i, j, sourceNode, targetNode, g;
+    var mapper = {};
 
-    // s = source, t = target
     if (d.source) {
       i = d.source.index;
       j = d.target.index;
-      s = _.where(mpr, {id: i });
-      t = _.where(mpr, {id: j });
-      // m.cheesecake = "hello";
-      m.sname = s[0].name;
-      m.sdata = d.source.value;
-      m.svalue = +d.source.value;
-      m.stotal = _.reduce(matrix[i], function (k, n) { return k + n; }, 0);
-      m.tname = t[0].name;
-      m.tdata = d.target.value;
-      m.tvalue = +d.target.value;
-      m.ttotal = _.reduce(matrix[j], function (k, n) { return k + n; }, 0);
+      sourceNode = _.where(mpr, {id: i });
+      // console.log(s);
+      targetNode = _.where(mpr, {id: j });
+      // console.log(t);
+      mapper.sourceName = sourceNode[0].name;
+      mapper.sourceData = d.source.value;
+      mapper.sourceValue = +d.source.value;
+      mapper.stotal = _.reduce(matrix[i], function (k, n) { return k + n; }, 0);
+      mapper.targetName = targetNode[0].name;
+      mapper.targetData = d.target.value;
+      mapper.targetValue = +d.target.value;
+      mapper.ttotal = _.reduce(matrix[j], function (k, n) { return k + n; }, 0);
     } else {
       g = _.where(mpr, {id: d.index });
-      m.gname = g[0].name;
-      m.gdata = g[0].data;
-      m.gvalue = d.value;
+      mapper.gname = g[0].name;
+      mapper.gdata = g[0].data;
+      mapper.gvalue = d.value;
     }
-    m.mtotal = _.reduce(matrix, function (m1, n1) {
+    mapper.mtotal = _.reduce(matrix, function (m1, n1) {
       return m1 + _.reduce(n1, function (m2, n2) { return m2 + n2; }, 0);
     }, 0);
-    return m;
+    return mapper;
   };
 }
